ab11613a9a3be2f198de80f3310503321cc709da
AlexSheer <alex_sheer@inbox.lv>
2014-12-21 23:44:06 +0200

    Update reset_styles.php

--- language/ru/tools/support/reset_styles.php
+++ language/ru/tools/support/reset_styles.php
@@ -1,3 +1,16 @@
+<?php
+/**
+*
+* @package Support Toolkit - Reset Styles Russian language Pthelovod
+* @version $Id$
+* @copyright (c) 2009 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
