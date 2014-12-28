3ed68e8dcf54c3b5722a3baca1cc835b73a807fa
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 18:05:33 +0200

    Update remove_duplicate_permissions.php

--- language/ru/tools/support/remove_duplicate_permissions.php
+++ language/ru/tools/support/remove_duplicate_permissions.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Support Toolkit - Duplicate Permission Remover  Russain language Pthelovod
+* @package Support Toolkit - Duplicate Permission Remover  Russain language Sheer
 * @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
