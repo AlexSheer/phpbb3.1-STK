04f0e3b88a30b3f02e8f14678e8572d51c3bf057
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 17:55:00 +0200

    Update resync_avatars.php

--- language/ru/tools/admin/resync_avatars.php
+++ language/ru/tools/admin/resync_avatars.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *
- * @package Support Toolkit - Resync Avatars Russian language Pthelovod
+ * @package Support Toolkit - Resync Avatars Russian language Sheer
  * @copyright (c) 2009 phpBB Group
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  *
