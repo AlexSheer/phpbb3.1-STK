7aa8a3457c9dbd213c527d3a60e24566f6d6cc91
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 18:05:05 +0200

    Update reclean_usernames.php

--- language/ru/tools/support/reclean_usernames.php
+++ language/ru/tools/support/reclean_usernames.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Support Toolkit - Reclean Usernames Russian language Pthelovod
+* @package Support Toolkit - Reclean Usernames Russian language Sheer
 * @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
