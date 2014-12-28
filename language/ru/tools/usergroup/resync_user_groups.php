af5e209af88e4f2cadf15fffde319e47db21f8ae
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 18:18:17 +0200

    Update resync_user_groups.php

--- language/ru/tools/usergroup/resync_user_groups.php
+++ language/ru/tools/usergroup/resync_user_groups.php
@@ -1,10 +1,10 @@
 <?php
 /**
  *
- * @package Support Toolkit - Resynchronise Registered users groups Russian language Pthelovod
+ * @package Support Toolkit - Resynchronise Registered users groups Russian language Sheer
  * @copyright (c) 2009 phpBB Group
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * 
+ *
  */
 
 /**
