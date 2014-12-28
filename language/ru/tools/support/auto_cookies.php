1bb2646aea41d1d9d3ecdf3438c12375097c5092
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 17:57:08 +0200

    Update auto_cookies.php

--- language/ru/tools/support/auto_cookies.php
+++ language/ru/tools/support/auto_cookies.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Support Toolkit - Auto Cookies Russian language Pthelovod
+* @package Support Toolkit - Auto Cookies Russian language Sheer
 * @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
