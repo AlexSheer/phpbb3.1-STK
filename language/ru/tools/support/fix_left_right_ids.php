81d92898e23749750137a38640784b0b9971923d
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 17:57:55 +0200

    Update fix_left_right_ids.php

--- language/ru/tools/support/fix_left_right_ids.php
+++ language/ru/tools/support/fix_left_right_ids.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Support Toolkit - Fix Left/Right ID's Russian language Pthelovod
+* @package Support Toolkit - Fix Left/Right ID's Russian language Sheer
 * @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
