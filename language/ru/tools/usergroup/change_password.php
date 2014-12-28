0a25e29bd265b78da1f14a5c4f826b4e96d64632
AlexSheer <alex_sheer@inbox.lv>
2014-12-28 18:09:18 +0200

    Update change_password.php

--- language/ru/tools/usergroup/change_password.php
+++ language/ru/tools/usergroup/change_password.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Support Toolkit - Change Password Russian language Pthelovod
+* @package Support Toolkit - Change Password Russian language Sheer
 * @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
