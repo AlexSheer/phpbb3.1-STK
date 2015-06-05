5de5fea4e24fca51ca2ce704db69ffeb44b26f49
AlexSheer <alex_sheer@inbox.lv>
2015-06-06 01:19:20 +0300

    Update reclean_usernames.php

--- tools/support/reclean_usernames.php
+++ tools/support/reclean_usernames.php
@@ -35,7 +35,7 @@ class reclean_usernames
 	*/
 	function run_tool()
 	{
-		global $db, $template;
+		global $db, $template, $user, $phpbb_root_path, $phpEx;
 
 		$part = request_var('part', 0);
 		$limit = 500;
@@ -50,6 +50,18 @@ class reclean_usernames
 
 			if ($username_clean != $row['username_clean'])
 			{
+				$sql = 'SELECT user_id, username, username_clean FROM ' . USERS_TABLE . ' WHERE username_clean LIKE \'' . $username_clean . '\'';
+				$res = $db->sql_query_limit($sql, 1);
+				$duplicate = $db->sql_fetchrow($res);
+				$db->sql_freeresult($res);
+
+				if (!empty($duplicate ))
+				{
+					$url = append_sid("{$phpbb_root_path}adm/index.$phpEx", 'i=users&amp;mode=overview&amp;u=' . $duplicate['user_id'] . '&amp;sid='. $user->data['session_id']);
+					$problem = append_sid("{$phpbb_root_path}adm/index.$phpEx", 'i=users&amp;mode=overview&amp;u=' . $row['user_id'] . '&amp;sid='. $user->data['session_id']);
+					trigger_error(sprintf($user->lang['USER_ALREADY_EXISTS'], $duplicate['username'], $url, $row['username'], $problem), E_USER_WARNING);
+				}
+
 				$db->sql_query('UPDATE ' . USERS_TABLE . " SET username_clean = '$username_clean' WHERE user_id = {$row['user_id']}");
 			}
 		}
