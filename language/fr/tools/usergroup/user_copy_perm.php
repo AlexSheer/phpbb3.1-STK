793644593198c848703d24dd94929a753d171c5f
Raphaël M. <Galixte@users.noreply.github.com>
2015-08-12 23:51:37 +0200

    Update of FRENCH translation for STK 3.1

--- language/fr/tools/usergroup/user_copy_perm.php
+++ language/fr/tools/usergroup/user_copy_perm.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package Support Toolkit - UserCopy Permissions
+* @package Support Toolkit - User Copy Permissions
 * @version $Id$
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
