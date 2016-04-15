de1f61d405c82df42c8c8ce35474fd8dce3fbb01
AlexSheer <alex_sheer@inbox.lv>
2016-04-15 23:42:23 +0300

    Upd
    Upd

--- includes/database_cleaner/functions_database_cleaner.php
+++ includes/database_cleaner/functions_database_cleaner.php
@@ -459,6 +459,7 @@ function fetch_cleaner_data(&$data, $phpbb_version)
 		case '3_1_7'	:
 		case '3_1_7_pl1':
 		case '3_1_8'	:
+		case '3_1_9'	:
 			// The extension group names have been changed, remove the old ones
 			foreach ($data->extension_groups as $key => $null)
 			{
